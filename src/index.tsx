750ab73ad3d3cdbbcbd62af681d6e831fcd4122d
Alexey Kopejchik <aleksei.k@helioscompanies.com>
2023-05-16 10:22:57 +0300

    Add basic route

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { Provider } from 'react-redux'
+import App from './app/App';
 import store from './store/store'
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>404</div>
+  },
+  {
+    path: "/hero/:id",
+    element: <div>Hero page</div>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +28,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
